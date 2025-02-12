2cd665d14a697bfa6fb1e19533e8026fe439e697
Vik Boyechko <vik.boyechko@gmail.com>
2025-02-12 01:02:30 -0600

    start

--- README.md
+++
@@ -1,1 +0,0 @@
-# lpcreate2
---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Loupedeck CT Profile Final Cut Pro",
+    "email": "",
+    "phoneForTel": "",
+    "phoneFormatted": "",
+    "address": {
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "",
+        "state": "",
+        "zip": "",
+        "country": "",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://fcpxprofile.com"
+};
